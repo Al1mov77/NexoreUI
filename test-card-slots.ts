76ab099718016e5136abbd4907bb410e3f3ce19d
Al1mov77 <umarlegaaas444@gmail.com>
2026-08-25 17:17:20 +0500

    chore(repo): update .gitignore and purge temporary build artifacts and sensitive logs

--- test-card-slots.ts
+++
@@ -1,6 +0,0 @@
-import { Card, CardHeader, CardTitle, CardContent } from './packages/ui/src/index';
-
-console.log("Card:", !!Card);
-console.log("CardHeader:", !!CardHeader);
-console.log("CardTitle:", !!CardTitle);
-console.log("CardContent:", !!CardContent);
